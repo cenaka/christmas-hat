03cc656cd67cba7f612356c48e8fd389b236dfb3
cenaka <cenaka@yandex.ru>
2015-11-27 19:44:48 +0300

    2

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -3,8 +3,8 @@ import copy
 from itertools import permutations
 from flask import render_template, flash, redirect, request, jsonify, url_for
 from FlaskWebProject import app
-
-result = []
+import pickle
+import ast
 
 
 @app.route('/')
@@ -14,26 +14,29 @@ def input_form():
 
 @app.route('/name', methods=['POST'])
 def get_names():
-    global result
     names = request.form['names'].split(",")
     not_to_names = request.form['not_to_names'].split(",")
     result = chistmas_hat(names, not_to_names)
     result = dict(zip(names, result))
-    return render_template("input_form.html")
+    write_to_file(result)
+    return redirect(render_template("christmas_hat.html",
+                           names=list(result.keys())), code=307)
 
 
 @app.route("/christmas_hat")
 def public_page():
+    result = read_from_file()
     return render_template("christmas_hat.html",
-                           number_of_people=len(result),
                            names=list(result.keys()))
 
 
 @app.route("/secret_santa", methods=["POST"])
 def secret_santa():
     santa = request.form["name"]
+    result = read_from_file()
     person = result[santa]
     del result[santa]
+    write_to_file(result)
     return render_template("result.html", name=person)
 
 
@@ -52,3 +55,19 @@ def chistmas_hat(people, not_to_people):
         if flag:
             return list(shuffle_names)
     return None
+
+
+def write_to_file(value, filepath="FlaskWebProject/static/result.pkl"):
+    with open(filepath, 'wb') as f:
+        pickle.dump(value, f)
+
+def read_from_file(filepath="FlaskWebProject/static/result.pkl"):
+    with open(filepath, 'rb') as f:
+        value = pickle.load(f)
+    return value
+
+if __name__ == "__main__":
+    filepath = "static/result.pkl"
+    write_to_file(filepath, str({"a":1, "b":2}))
+    value = read_from_file(filepath)
+    print(ast.literal_eval(value))
--- runserver.py
+++ runserver.py
@@ -11,9 +11,5 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    import logging
-    from logging import FileHandler
-    file_handler = FileHandler('log.txt')
-    file_handler.setLevel(logging.DEBUG)
-    app.logger.addHandler(file_handler)
-    app.run(HOST, PORT)
+
+    app.run(HOST, PORT, debug=True)
